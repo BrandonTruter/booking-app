4353dc1a39e5c8774fb9604cdb0e6325b28238f6
BrandonTruter <brandonleetruter@gmail.com>
2024-04-26 12:04:41 +0200

    increase test coverage

--- spec/requests/diaries_spec.rb
+++ spec/requests/diaries_spec.rb
@@ -3,16 +3,8 @@ require 'rails_helper'
 RSpec.describe "Diaries", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/diaries/index"
+      get diaries_path
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/diaries/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
--- spec/requests/entities_spec.rb
+++ spec/requests/entities_spec.rb
@@ -3,16 +3,8 @@ require 'rails_helper'
 RSpec.describe "Entities", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/entities/index"
+      get "/entities"
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/entities/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
