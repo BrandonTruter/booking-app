528dd724e821908117e2e96dcf4334440ee25df8
BrandonTruter <brandonleetruter@gmail.com>
2024-04-27 10:23:28 +0200

    [wip] update config

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,17 +6,25 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    goodx = 'https://dev_interview.qagoodx.co.za'
-
-    policy.connect_src :self, '*'
-    policy.script_src :self, goodx
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
-    policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https
-    
+    # gxweb = 'https://dev_interview.qagoodx.co.za'
+    policy.default_src :none
+    policy.script_src :http, :https, 'unsafe-inline' #, gxweb
+    policy.style_src :http, :https, 'unsafe-inline'
+    policy.img_src :http, :https, :data
+    policy.font_src :http, :https
+    policy.connect_src :http, :https
+    policy.media_src :http, :https
+    policy.object_src :none
+    policy.child_src :none
+    policy.frame_src :none
+    # policy.connect_src :self, '*'
+    # policy.script_src :self, gxweb
+    # policy.default_src :self, :https
+    # policy.font_src    :self, :https, :data
+    # policy.img_src     :self, :https, :data
+    # policy.object_src  :none
+    # policy.script_src  :self, :https
+    # policy.style_src   :self, :https
   end
 
   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
