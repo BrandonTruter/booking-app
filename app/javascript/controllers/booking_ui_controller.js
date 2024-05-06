75277bd8798d1e285356b0376169d98870788400
BrandonTruter <brandonleetruter@gmail.com>
2024-05-06 15:22:22 +0200

    Add logic to handle Booking resources
    Provides support to allocate `Bookings` for each diary.
    
    Bookings represent the core of the application as they expose full
    RESTFul services to manage all the scheduled diary appointments for a
    given entity.
    
    This commit incorporates the following:
    - Model for defining validations rules
    - View for rendering the data and forms
    - Controller for connecting system components
    - Migration for generating the database schema
    - Specs for testing the model, controller and API
    - Serializer to represent the requried data structures
    - Stimulus controller for handling client-side requests

--- app/javascript/controllers/booking_ui_controller.js
+++ app/javascript/controllers/booking_ui_controller.js
@@ -5,71 +5,15 @@ export default class extends Controller {
   connect() {
     console.log('CONNECTED BOOKING API');
 
-    // axios.get('/api/v1/bookings')
-    //   .then(function (response) {
-    //     console.log(response, "BOOKINGS API");
-    //     // this.tableTarget.outerHTML =
-    //     // document.querySelector('#bookings').innerHTML = `<table>
-    //     //   <thead>
-    //     //      <tr>
-    //     //         <th>UID</th>
-    //     //         <th>Entity</th>
-    //     //         <th>Booking Type</th>
-    //     //         <th>Name</th>
-    //     //         <th>UUID</th>
-    //     //         <th>Disabled</th>
-    //     //      </tr>
-    //     //    </thead>
-    //     //    <tbody data-target="table.body">
-    //     //       ${response.data.forEach(resp => {
-    //     //         `<tr data-target="table.row">
-    //     //           <td data-target="table.cell">${resp.uid}</td>
-    //     //           <td data-target="table.cell">${resp.entity_uid}</td>
-    //     //           <td data-target="table.cell">${resp.booking_type_uid}</td>
-    //     //           <td data-target="table.cell">${resp.name}</td>
-    //     //           <td data-target="table.cell">${resp.uuid}</td>
-    //     //           <td data-target="table.cell">${resp.disabled}</td>
-    //     //         </tr>`
-    //     //       })}
-    //     //     </tbody>
-    //     //   </table>`
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   })
-    //   .finally(function () {
-    //     // always executed
-    //   })
-
-    //   axios.get('/api/v1/debtors').then(function (response) {
-    //     console.log(response, "DEBBTORS API");
-    //     // document.querySelector('#debtors').innerHTML = `<table>
-    //     //   <thead>
-    //     //      <tr>
-    //     //         <th>UID</th>
-    //     //         <th>Entity</th>
-    //     //         <th>Name</th>
-    //     //         <th>Surname</th>
-    //     //      </tr>
-    //     //    </thead>
-    //     //    <tbody class="table-body">
-    //     //       ${response.data.forEach((resp) => {
-    //     //         return `<tr class="table-row">
-    //     //           <td class="table-cell">${resp.uid}</td>
-    //     //           <td class="table-cell">${resp.entity_uid}</td>
-    //     //           <td class="table-cell">${resp.name}</td>
-    //     //           <td class="table-cell">${resp.surname}</td>
-    //     //         </tr>`
-    //     //       })}
-    //     //     </tbody>
-    //     //   </table>`
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   })
-    //   .finally(function () {
-    //     // always executed
-    //   });
-
+    axios.get('/api/v1/bookings')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      })
   }
 }
