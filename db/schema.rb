e6fe1fa41292306e1cef9f9cd91b55143141e18c
BrandonTruter <brandonleetruter@gmail.com>
2024-05-20 08:38:20 +0200

    Update DB schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_26_143809) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_042309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "booking_types", force: :cascade do |t|
+    t.uuid "uuid"
+    t.string "name"
+    t.string "color"
+    t.integer "duration"
+    t.integer "diary_uid"
+    t.integer "entity_uid"
+    t.integer "booking_uid"
+    t.integer "booking_status_uid"
+    t.string "bookable_type"
+    t.bigint "bookable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bookable_type", "bookable_id"], name: "index_booking_types_on_bookable"
+    t.index ["diary_uid", "booking_uid"], name: "index_booking_types_on_diary_uid_and_booking_uid"
+  end
+
   create_table "bookings", force: :cascade do |t|
     t.text "reason"
-    t.string "booking_type"
-    t.string "status"
     t.datetime "end_at"
-    t.datetime "start_at"
+    t.datetime "start_time"
     t.integer "duration"
     t.integer "debtor_id"
     t.integer "patient_id"
     t.bigint "diary_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
+    t.boolean "cancelled"
+    t.uuid "uuid"
+    t.integer "entity_uid"
+    t.integer "booking_type_uid"
+    t.integer "booking_status_uid"
     t.index ["diary_id"], name: "index_bookings_on_diary_id"
   end
 
@@ -41,16 +62,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_143809) do
   end
 
   create_table "diaries", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.date "start_date"
-    t.date "end_date"
+    t.string "name"
     t.integer "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "set_default", default: true
-    t.uuid "external_id"
+    t.boolean "disabled", default: true
+    t.uuid "uuid"
     t.bigint "entity_id"
+    t.integer "treating_doctor_uid"
+    t.integer "booking_type_uid"
+    t.json "booking_statuses"
+    t.json "booking_types"
+    t.integer "uid"
   end
 
   create_table "entities", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,99 @@
+# ENTITY
 entity = Entity.create!(
+  title: 'APP 005',
   username: Rails.application.credentials.gxweb.username,
   password: Rails.application.credentials.gxweb.password,
-  description: 'For interfacing with GoodX via an API that is part of the GoodX web',
-  title: 'APP 005'
+  description: 'For interfacing with GoodX via an API that is part of the GoodX web'
 )
 
-diary = Diary.create!(
-  title: '05 Diary',
-  start_date: '2023-11-01',
-  end_date: '2023-12-01',
-  entity_id: entity.id
-)
+# DIARIES
+diary_json = GxWeb::Client.new.list_diaries
+if diary_json && diary_json.key?(:data)
+  diary_json[:data].each do |record|
+    booking_types = []
+    booking_states = []
 
-booking = Booking.create!(
-  reason: 'strictly testing',
-  start_at: diary.start_date,
-  end_at: diary.end_date,
-  diary_id: diary.id,
-  status: "Booked",
-  booking_type: "Meeting"
-)
+    record[:booking_types].each do |bt|
+      booking_types << {
+        uid: bt[:uid],
+        name: bt[:name],
+        uuid: bt[:uuid],
+        color: bt[:color]
+      }
+    end
+
+    record[:booking_statuses].each do |bt|
+      booking_states << {
+        uid: bt[:uid],
+        name: bt[:name],
+        uuid: bt[:uuid],
+        color: bt[:color]
+      }
+    end
+
+    Diary.create!(
+      name: record[:name],
+      uid: record[:uid],
+      uuid: record[:uuid],
+      entity_id: entity.id,
+      treating_doctor_uid: record[:treating_doctor_uid],
+      booking_type_uid: record[:booking_type_uid],
+      disabled: record[:disabled],
+      booking_types: booking_types,
+      booking_statuses: booking_states,
+    )
+  end
+end
+
+# PATIENTS
+patients_json = GxWeb::Client.new.list_patients
+if patients_json && patients_json.key?(:data)
+  patients_json[:data].each do |record|
+    Patient.create!(
+      name: record[:surname],
+      uid: record[:uid],
+      debtor_uid: record[:debtor_uid],
+      booking: Booking.last
+    )
+  end
+end
+
+# DEBTORS
+debtors_json = GxWeb::Client.new.list_debtors
+if debtors_json && debtors_json.key?(:data)
+  debtors_json[:data].each do |record|
+    Debtor.create!(
+      name: record[:surname],
+      uid: record[:uid],
+      patient_uids: record[:patients].map {|p| p[:uid]}.join(','),
+      booking: Booking.last
+    )
+  end
+end
+
+# BOOKINGS
+booking_json = GxWeb::Client.new.list_bookings
+if booking_json && booking_json.key?(:data)
+  booking_json[:data].each do |record|
+    diary = Diary.find_by(uid: record[:diary_uid])
+    next unless diary.present?
+
+    debtor = Debtor.find_or_initialize_by(uid: record[:patient_uid])
+    patient = Patient.find_or_initialize_by(uid: record[:patient_uid])
+
+    Booking.create!(
+      reason: record[:reason] || "first test round",
+      duration: record[:duration],
+      start_time: record[:start_time],
+      uid: record[:uid],
+      uuid: record[:uuid],
+      entity_uid: record[:entity_uid],
+      booking_type_uid: record[:booking_type_uid],
+      booking_status_uid: record[:booking_status_uid],
+      cancelled: record[:cancelled],
+      diary_id: diary&.id,
+      debtor_id: debtor&.id,
+      patient_id: patient&.id
+    )
+  end
+end
